0948cdaa76e96d859f6b7f989a1b1d75cb8b0e15
vzaidman <vzaidman@gmail.com>
2017-03-02 12:26:45 +0200

    made the plugin to list all png files

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ function FileListPlugin(options) {}
 
 FileListPlugin.prototype.apply = function(compiler) {
   compiler.plugin('emit', function(compilation, callback) {
+
+    const prefetchList = Object.keys(compilation.assets)
+      .filter(function(asset){
+        return /png/.test(asset)
+      })
+      .join('\n')
+
+    compilation.assets['prefetch-list.txt'] = {
+      source: function() {
+        return prefetchList;
+      },
+      size: function() {
+        return prefetchList.length;
+      }
+    };
+
     callback();
   });
 };
