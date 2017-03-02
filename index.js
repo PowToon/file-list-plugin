c507d04d8dea9e00a681f2524dc5afbf0dabaf15
vzaidman <vzaidman@gmail.com>
2017-03-02 16:11:24 +0200

    improved plugin to consider chunks and removed unused dependencies

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
-var URI = require('urijs')
-
 var flatMap = require('lodash.flatmap')
 var keys = require('lodash.keys')
 var defaults = require('lodash.defaults')
 
+var defaultOptions = {
+  fileName: 'asset-list.txt',
+  filterChunks: function defaultFilterChunks(chunk){
+    return true
+  },
+  filterModules: function defaultFilterModules(module){
+    return true
+  },
+  mapModules: function defaultMapModules(module){
+    return keys(module.assets)
+  },
+  format: function defaultFormat(listItems){
+    return listItems.join('\n')
+  }
+}
+
 function FileListPlugin(options) {
-  defaults(this, options, {
-    fileName: 'asset-list.txt',
-
-    filterModules: function defaultFilterModules(module){
-      return URI(module.rawRequest).hasQuery('load', 'prefetch')
-    },
-
-    mapModules: function defaultMapModules(module){
-      return keys(module.assets)
-    },
-
-    format: function defaultFormat(listEntries){
-      return listEntries.join('\n')
-    }
-  })
+  defaults(this, options, defaultOptions)
 }
 
 FileListPlugin.prototype.apply = function(compiler) {
+  var fileName = this.fileName
+  var filterChunks = this.filterChunks
   var filterModules = this.filterModules
   var mapModules = this.mapModules
   var format = this.format
-  var fileName = this.fileName
 
-  compiler.plugin('this-compilation', function(compilation) {
+  compiler.plugin('compilation', function(compilation) {
+    compiler.plugin('additional-chunk-assets', function(callback){
+      callback()
+    })
+
     compilation.plugin('additional-assets', function(callback) {
-      var modulesToList = compilation.modules.filter(filterModules)
-      var assetsToList = flatMap(modulesToList, mapModules)
-      var list = format(assetsToList)
+
+      var chunks = compilation.chunks.filter(filterChunks)
+
+      var modules = flatMap(chunks, function(chunk){
+        return chunk.modules
+      }).filter(filterModules)
+
+      var listItems = flatMap(modules, mapModules)
+
+      var list = format(listItems)
 
       if(typeof(list) !== 'string'){
         list = JSON.stringify(list)
@@ -53,3 +65,4 @@ FileListPlugin.prototype.apply = function(compiler) {
 }
 
 module.exports = FileListPlugin
+module.exports.defaultOptions = defaultOptions
